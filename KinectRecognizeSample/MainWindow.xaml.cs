4df041c5d6a745c32690dc1960cbeec5eb56827f
ichinoseShugo <ichinose@srmtlab.org>
2018-10-19 19:22:16 +0900

    カラー画像の表示

--- KinectRecognizeSample/MainWindow.xaml.cs
+++ KinectRecognizeSample/MainWindow.xaml.cs
@@ -13,6 +13,8 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+using Microsoft.Kinect;
+
 namespace KinectRecognizeSample
 {
     /// <summary>
@@ -20,9 +22,79 @@ namespace KinectRecognizeSample
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        KinectSensor kinect;
+
+        ColorFrameReader colorFrameReader;
+        FrameDescription colorFrameDesc;
+        byte[] colorBuffer;
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                // Kinectを開く
+                kinect = KinectSensor.GetDefault();
+                if (kinect == null)
+                {
+                    throw new Exception("Kinectを開けません");
+                }
+
+                kinect.Open();
+
+                // カラー画像の情報を作成する(BGRAフォーマット)
+                colorFrameDesc = kinect.ColorFrameSource.CreateFrameDescription(ColorImageFormat.Bgra);
+
+                // カラーリーダーを開く
+                colorFrameReader = kinect.ColorFrameSource.OpenReader();
+                colorFrameReader.FrameArrived += colorFrameReader_FrameArrived;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                Close();
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            // 終了処理
+            if (colorFrameReader != null)
+            {
+                colorFrameReader.Dispose();
+                colorFrameReader = null;
+            }
+
+            if (kinect != null)
+            {
+                kinect.Close();
+                kinect = null;
+            }
+        }
+
+        void colorFrameReader_FrameArrived(object sender, ColorFrameArrivedEventArgs e)
+        {
+            // カラーフレームを取得する
+            using (var colorFrame = e.FrameReference.AcquireFrame())
+            {
+                if (colorFrame == null)
+                {
+                    return;
+                }
+
+                // BGRAデータを取得する
+                colorBuffer = new byte[colorFrameDesc.Width * colorFrameDesc.Height * colorFrameDesc.BytesPerPixel];
+                colorFrame.CopyConvertedFrameDataToArray(colorBuffer, ColorImageFormat.Bgra);
+
+                // ビットマップにする
+                ImageColor.Source = BitmapSource.Create(colorFrameDesc.Width, colorFrameDesc.Height, 96, 96,
+                    PixelFormats.Bgra32, null, colorBuffer, colorFrameDesc.Width * (int)colorFrameDesc.BytesPerPixel);
+            }
         }
     }
 }
